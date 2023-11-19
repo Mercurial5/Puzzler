8e3db93455c014abfe917f0ef164434956a5760b
yukishiro <IliyasNeespayev@gmail.com>
2023-11-19 22:09:38 +0600

    add programming questions

--- src/configs.py
+++ src/configs.py
@@ -6,6 +6,26 @@ load_dotenv()
 
 BOT_TOKEN = environ['BOT_TOKEN']
 
+PROGRAMMING_QUESTIONS = {
+    1: {
+        'prompt': 'where is correct if?\n'
+                  '\n'
+                  '1. <if></if>\n'
+                  '2. <if><if>\n'
+                  '3. if ()\n'
+                  '4. if ( ) { }',
+        'answer': '4',
+    },
+    2: {
+        'prompt': 'where is correct div?\n'
+                  '\n'
+                  '1. <div></div>\n'
+                  '2. <div><div>\n'
+                  '3. div/div\n'
+                  '4. div{}\n',
+        'answer': '1',
+    }
+}
 MATH_QUESTIONS = {
     1: {
         'prompt': 'x = 5 - 3. What is x?',
