852449e67ea047a20a339eabaff3d11b540a0045
yukishiro <IliyasNeespayev@gmail.com>
2023-11-19 18:19:18 +0600

    add more questions

--- src/configs.py
+++ src/configs.py
@@ -10,5 +10,13 @@ MATH_QUESTIONS = {
     1: {
         'prompt': 'x = 5 - 3. What is x?',
         'answer': '2'
+    },
+    2: {
+        'prompt': 'x = 2y/2. What is x?',
+        'answer': 'y'
+    },
+    3: {
+        'prompt': 'x = 6 - 2. What is x?',
+        'answer': '4'
     }
 }
